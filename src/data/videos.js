e44a0081e17607cee8cc8ac9233b511e55b53d2c
Md Aminul Islam Sayem <60499172+sayem941@users.noreply.github.com>
2026-01-22 22:04:05 +0600

    Update videos.js

--- src/data/videos.js
+++ src/data/videos.js
@@ -4,13 +4,13 @@ import thumb2 from "../assets/Cinema.png";
 const videos = [
   {
     id: 1,
-    title: "Wedding Trailor from YouTube",
+    title: "Wedding Trailor",
     videoUrl: "https://www.youtube.com/embed/QVgcwf1EGyU",
     thumbnail: thumb1,
   },
   {
     id: 2,
-    title: "Wedding Movie from Drive",
+    title: "Wedding Cinema",
     videoUrl: "https://drive.google.com/file/d/1yFwsJa3DUlGhXpuTE5BZaLg1sgHNpFoq/preview",
     thumbnail: thumb2,
   },

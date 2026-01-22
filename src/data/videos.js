6a04d219c2cba012c4207833dae7489e59d01798
Md Aminul Islam Sayem <60499172+sayem941@users.noreply.github.com>
2026-01-22 20:56:47 +0600

    Thumbnail issue solved

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,15 +1,18 @@
+import thumb1 from "../assets/Cinema.png";
+import thumb2 from "../assets/Trailer.png";
+
 const videos = [
   {
     id: 1,
     title: "Wedding Trailor from YouTube",
     videoUrl: "https://www.youtube.com/embed/QVgcwf1EGyU",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1rY6fI8b-zdC8hUBGyKD2KmjcdRjY3f5K",
+    thumbnail: thumb1,
   },
   {
     id: 2,
     title: "Wedding Movie from Drive",
     videoUrl: "https://drive.google.com/file/d/1yFwsJa3DUlGhXpuTE5BZaLg1sgHNpFoq/preview",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1-o8MWSxfY-oBvIILTgOaJRHR8HAXPlrD",
+    thumbnail: thumb2,
   },
 ];
 
